eba7d2b57cf5520f9fba7d2bce119d5aff0054c2
Raki <mikoto0931@gmail.com>
2021-04-07 23:01:48 +0900

    v3 branch
    BubbleDrive v3

--- BubbleDrive8_ModelSim/W25Q32JVxxIM.v
+++
@@ -1,9 +0,0 @@
-/******************************************************************************
- Winbond Electronics Corporation
- Verilog Model for W25Q32JVxxIM Serial Flash Memory
-
- V1.2-beta
-
-Download W25Q32JVxxIM.v from Winbond website and paste here.
-
-******************************************************************************/
