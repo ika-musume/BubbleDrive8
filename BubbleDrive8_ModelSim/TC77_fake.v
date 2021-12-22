71679afaeb696741a0d199bc17ad7a043139bc4d
Raki <mikoto0931@gmail.com>
2021-12-22 13:03:35 +0900

    revise TC77 model

--- BubbleDrive8_ModelSim/TC77_fake.v
+++ BubbleDrive8_ModelSim/TC77_fake.v
@@ -12,46 +12,44 @@ module TC77_fake
     input   wire            nSYSOK
 );
 
+reg     [15:0]  SERIALREG;
+assign  #7    SIO = (nCS == 1'b1) ? 1'bZ : SERIALREG[15];
 
-reg     [12:0]  TEMP_VALUE = 13'b0_0001_1001_0000; //25 degrees
-reg             IS_CONVERTED = 1'b0; //converted
-localparam FILLER = 2'bZZ;
-
-always @(negedge nSYSOK)
+//temperature list
+reg     [15:0]  TEMPERATURE_LIST [3:0];
+initial 
 begin
-    #0 TEMP_VALUE <= 13'b0_0001_1001_0000;  //not ready
-    #0 IS_CONVERTED <= 1'b0;
-
-    #4200 TEMP_VALUE <= 13'b0_0001_0000_0000; //16 degrees
-    #0 IS_CONVERTED <= 1'b1;
-
-    #4000 TEMP_VALUE <= 13'b0_0010_0100_0000; //36 degrees
-    #0 IS_CONVERTED <= 1'b1;
-
-    #53420 TEMP_VALUE <= 13'b0_0001_1000_0000; //24 degrees
-    #0 IS_CONVERTED <= 1'b1;
-
-    #22400 TEMP_VALUE <= 13'b0_0010_0111_0000; //39 degrees
-    #0 IS_CONVERTED <= 1'b1;
+    TEMPERATURE_LIST[0] <= 16'b0_0001_1001_0000_0_ZZ; //not ready
+    TEMPERATURE_LIST[1] <= 16'b1_0001_0000_0000_1_ZZ; //negative retry
+    TEMPERATURE_LIST[2] <= 16'b0_0001_0000_0000_1_ZZ; //16
+    TEMPERATURE_LIST[3] <= 16'b0_0010_0100_0000_1_ZZ; //36
+    TEMPERATURE_LIST[4] <= 16'b0_0001_1000_0000_1_ZZ; //24
+    TEMPERATURE_LIST[5] <= 16'b0_0010_0111_0000_1_ZZ; //39
 end
 
 
-reg     [15:0]  tempreg = 16'b0;
-reg             OUTLATCH = 1'bZ;
-assign  #2    SIO = (nCS == 1'b1) ? 1'bZ : OUTLATCH;
+reg     [3:0]   LISTCOUNTER = 4'd15;
 
-always @(*)
+always @(posedge nCS)
 begin
-    if(nCS == 1'b1)
+    if(LISTCOUNTER == 4'd15)
     begin
-        tempreg <= {TEMP_VALUE, IS_CONVERTED, FILLER};
+        LISTCOUNTER <= 4'd0;
+    end
+    else
+    begin
+        LISTCOUNTER <= LISTCOUNTER + 4'd1;
     end
 end
 
-always @(negedge nCS or negedge CLK)
+always @(negedge nCS)
 begin
-    OUTLATCH <= #8 tempreg[15];
-    tempreg[15:1] <= #8 tempreg[14:0];
+    SERIALREG <= TEMPERATURE_LIST[LISTCOUNTER];
+end
+
+always @(negedge CLK)
+begin
+    SERIALREG[15:1] <= #3 SERIALREG[14:0];
 end
 
 endmodule
